453bf1e3e6710f23f0b1e4994761c85373a50ce1
SUCCESS <khalid.saifullah0012@gmail.com>
2024-09-12 22:43:34 +0600

    Added new codes in supplier class

--- src/main/java/com/microtech/walmartapp/entity/Supplier.java
+++ src/main/java/com/microtech/walmartapp/entity/Supplier.java
@@ -10,4 +10,28 @@ public class Supplier {
     @GeneratedValue
     private int id;
     private String name;
+
+    public Supplier(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Supplier() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
