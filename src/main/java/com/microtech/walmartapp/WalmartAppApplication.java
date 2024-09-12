56650b91fbbf46c381a49a4cbe7cbbbc57f71c8d
SUCCESS <khalid.saifullah0012@gmail.com>
2024-09-12 21:40:12 +0600

    Created a repository

---
+++ src/main/java/com/microtech/walmartapp/WalmartAppApplication.java
@@ -0,0 +1,13 @@
+package com.microtech.walmartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalmartAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalmartAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microtech/walmartapp/WalmartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microtech.walmartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmartAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
