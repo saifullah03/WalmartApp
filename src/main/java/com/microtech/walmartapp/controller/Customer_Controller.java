b232cb678a88fc9b7ae600e354eeab39d48752ae
rezwanul91 <mohammadhaquer@gmail.com>
2024-09-16 13:07:00 -0400

    Created Repository

---
+++ src/main/java/com/microtech/walmartapp/controller/Customer_Controller.java
@@ -0,0 +1,11 @@
+package com.microtech.walmartapp.controller;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cust")
+public class Customer_Controller {
+}
